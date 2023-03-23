b82afcbf3135b5bd78b09735ebac2671ef12b516
Omer Michael <74428768+omerMichael@users.noreply.github.com>
2023-03-23 15:02:38 +0200

    Update Point.py

--- src/Point.py
+++ src/Point.py
@@ -8,4 +8,4 @@ class Point:
 
     def distance(self, other):
         # distance between
-        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2) 
